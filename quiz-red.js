7d5f3c315a8f5790b53e4d85256982626640eafc
Daniel Andrade <dcca12@gmail.com>
2019-05-15 01:14:57 +0200

    Setting views and basic JS

--- quiz-red.js
+++ quiz-red.js
@@ -1,1 +1,21 @@
-alert('CONECTED');
+//Acess JSON - https://api.myjson.com/bins/mxy0i
+var quizData = null;
+var jqxhr = $.getJSON("https://api.myjson.com/bins/mxy0i", function(result){
+  quizData = result.quizzes;
+})
+
+//Listenner
+$('.quiz').on('click', function(){
+  $('#home').addClass('hide');
+  $('#quiz').removeClass('hide');
+  if($(this).attr('id') === 'q1'){
+    $('#question').text(quizData[0].questions[0].question);
+  } else{
+    $('#question').text(quizData[1].questions[0].question)
+  }
+})
+
+$('.answers').on('click', function(){
+  $('#quiz').addClass('hide');
+  $('#result').removeClass('hide');
+})
