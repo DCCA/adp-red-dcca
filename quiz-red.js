ac42bb1e253c4a2f6ef6756c2ac1b39d9ae6639b
Daniel Andrade <dcca12@gmail.com>
2019-05-15 01:48:19 +0200

    Add questionLoad function

--- quiz-red.js
+++ quiz-red.js
@@ -3,19 +3,41 @@ var quizData = null;
 var jqxhr = $.getJSON("https://api.myjson.com/bins/mxy0i", function(result){
   quizData = result.quizzes;
 })
+var trackQuiz = 0;
+var trackQuestion = 0;
+var score = 0;
 
 //Listenner
 $('.quiz').on('click', function(){
   $('#home').addClass('hide');
   $('#quiz').removeClass('hide');
   if($(this).attr('id') === 'q1'){
-    $('#question').text(quizData[0].questions[0].question);
+    trackQuiz = 0;
+    loadQuestion();
   } else{
-    $('#question').text(quizData[1].questions[0].question)
+    trackQuiz = 1;
+    loadQuestion();
   }
 })
 
 $('.answers').on('click', function(){
-  $('#quiz').addClass('hide');
-  $('#result').removeClass('hide');
+  var resp = $(this).attr('id');
+  if(quizData[trackQuiz].questions[trackQuestion].answers[resp].value){
+    $(this).addClass('correct');
+    score++
+    trackQuestion++;
+    loadQuestion();
+  } else {
+    $(this).addClass('wrong');
+    trackQuestion++;
+    loadQuestion();
+  }
 })
+
+//Funtions
+function loadQuestion(){
+  $('#question').text(quizData[trackQuiz].questions[trackQuestion].question);
+  for(var i = 0; i < quizData[trackQuiz].questions[trackQuestion].answers.length ; i++){
+    $('#'+i).text(quizData[trackQuiz].questions[trackQuestion].answers[i].content);
+  }
+}
