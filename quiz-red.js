f84d539b3490d3718e7965fc60590e7fab829a34
Daniel Andrade <dcca12@gmail.com>
2019-05-15 03:01:55 +0200

    Finished the business rules

--- quiz-red.js
+++ quiz-red.js
@@ -1,11 +1,12 @@
 //Acess JSON - https://api.myjson.com/bins/mxy0i
-var quizData = null;
-var jqxhr = $.getJSON("https://api.myjson.com/bins/mxy0i", function(result){
+var quizData = null,
+    jqxhr = $.getJSON("https://api.myjson.com/bins/mxy0i", function(result){
   quizData = result.quizzes;
-})
-var trackQuiz = 0;
-var trackQuestion = 0;
-var score = 0;
+}),
+    trackQuiz = 0,
+    trackQuestion = 0,
+    score = 0,
+    visScore = 0;
 
 //Listenner
 $('.quiz').on('click', function(){
@@ -18,23 +19,51 @@ $('.quiz').on('click', function(){
     trackQuiz = 1;
     loadQuestion();
   }
-})
+});
 
 $('.answers').on('click', function(){
   var resp = $(this).attr('id');
-  if(quizData[trackQuiz].questions[trackQuestion].answers[resp].value){
-    $(this).addClass('correct');
-    score++
-    trackQuestion++;
-    loadQuestion();
-  } else {
-    $(this).addClass('wrong');
-    trackQuestion++;
-    loadQuestion();
-  }
-})
+  if(trackQuestion != 2){
+    var t = this;
+    checkingAnswer(this, resp);
+    setTimeout(function(){
+      loadQuestion();
+      $(t).removeClass('btn-success');
+      $(t).removeClass('btn-danger');
+    },2000);
+  } else{
+      checkingAnswer(this, resp);
+      setTimeout(function(){
+        resultPage();
+      },2000);
+  }})
 
 //Funtions
+function resultPage(){
+  $('#quiz').addClass('hide');
+  $('#result').removeClass('hide');
+  if(visScore > 0.5){
+    $('.message').text('Passou!');
+  } else{
+    $('.message').text('Bombou!');
+  }
+}
+
+function checkingAnswer(a, b){
+  if(quizData[trackQuiz].questions[trackQuestion].answers[b].value){
+    $(a).addClass('btn-success');
+    alert('Correct!');
+    score++
+    visScore = score/3;
+    $('.score').text(visScore);
+    trackQuestion++;
+  } else {
+    $(a).addClass('btn-danger');
+    alert('Wrong!');
+    trackQuestion++;
+  }
+  }
+
 function loadQuestion(){
   $('#question').text(quizData[trackQuiz].questions[trackQuestion].question);
   for(var i = 0; i < quizData[trackQuiz].questions[trackQuestion].answers.length ; i++){
